3f6f8a70a4f031c31fb02240eced303227b89cd6
Shadiya Iffath <32609286+ShadiyaIffath@users.noreply.github.com>
2023-12-12 23:03:14 +0000

    Size fixing (#10)
    * reveal tiles logic
    
    * win loose logic
    
    * resized board size

--- script.js
+++ script.js
@@ -8,8 +8,8 @@ import {
   revealAllMines,
 } from "./minesweeper.js";
 
-const BOARD_SIZE = 3;
-const NUMBER_OF_MINES = 1;
+const BOARD_SIZE = 10;
+const NUMBER_OF_MINES = 10;
 
 const board = createBoard(BOARD_SIZE, NUMBER_OF_MINES);
 
