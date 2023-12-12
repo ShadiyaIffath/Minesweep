b9658ea348f39257e24373568f55a0e515c48c89
Shadiya Iffath <shadiya.iffath98@gmail.com>
2023-12-12 18:17:46 +0000

    added logic to mark tiles

--- script.js
+++ script.js
@@ -1,15 +1,23 @@
-import { createBoard } from "./minesweeper.js";
+import { createBoard, markTile, minesMarkedCount } from "./minesweeper.js";
 
 const BOARD_SIZE = 10;
 const NUMBER_OF_MINES = 10;
 
 const board = createBoard(BOARD_SIZE, NUMBER_OF_MINES);
-
+console.log(board);
 const boardElement = document.querySelector(".board");
+const mineCountElement = document.querySelector("[mine-count]");
 
 board.forEach((row) => {
   row.forEach((tile) => {
     boardElement.append(tile.element);
+    tile.element.addEventListener("click", () => {});
+    tile.element.addEventListener("contextmenu", (event) => {
+      event.preventDefault();
+      markTile(tile);
+      mineCountElement.textContent = NUMBER_OF_MINES - minesMarkedCount();
+    });
   });
 });
 boardElement.style.setProperty("--size", BOARD_SIZE);
+mineCountElement.textContent = NUMBER_OF_MINES;
