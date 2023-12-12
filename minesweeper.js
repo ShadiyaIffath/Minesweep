3ca8e22b56d83370bf28f657d9c79764bd945600
Shadiya Iffath <32609286+ShadiyaIffath@users.noreply.github.com>
2023-12-12 17:00:38 +0000

    Setup tiles and mines for the minesweeper board (#5)

---
+++ minesweeper.js
@@ -0,0 +1,71 @@
+const TILE_STATUSES = {
+  HIDDEN: "hidden",
+  MINE: "mine",
+  MARKED: "marked",
+  NUMBER: "number",
+};
+
+export function createBoard(boardSize, numberOfMines) {
+  const board = [];
+  const minePositions = getMinePositions(boardSize, numberOfMines);
+
+  for (let x = 0; x < boardSize; x++) {
+    const row = [];
+    for (let y = 0; y < boardSize; y++) {
+      const element = document.createElement("div");
+      element.addEventListener("click", (mouseEvent) => {});
+      element.dataset.status = TILE_STATUSES.HIDDEN;
+
+      const tile = {
+        x,
+        y,
+        mine: minePositions.some((position) =>
+          isPositionAMatch(position, { x, y })
+        ),
+        element,
+        get status() {
+          return this.element.dataset.status;
+        },
+        set status(value) {
+          this.element.dataset.status = value;
+        },
+      };
+      row.push(tile);
+    }
+
+    board.push(row);
+  }
+
+  return board;
+}
+
+function getMinePositions(boardSize, numberOfMines) {
+  const positions = [];
+  while (positions.length < numberOfMines) {
+    const generatedPosition = {
+      x: randomNumber(boardSize),
+      y: randomNumber(boardSize),
+    };
+
+    if (
+      !positions.some((position) =>
+        isPositionAMatch(generatedPosition, position)
+      )
+    ) {
+      positions.push(generatedPosition);
+    }
+  }
+  return positions;
+}
+
+function isPositionAMatch(randomPosition, listedPosition) {
+  return (
+    randomPosition.x == listedPosition.x && randomPosition.y == listedPosition.y
+  );
+}
+
+function randomNumber(size) {
+  return Math.floor(Math.random() * size);
+}
+
+function attachEventListenerForTile() {}
